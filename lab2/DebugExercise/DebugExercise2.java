c8feb4b08ebb1b87f67a91c8167070b040c0d335
Kal'tsit <36351545+YinY1@users.noreply.github.com>
2022-09-01 19:14:26 +0800

    fixed bug

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -5,7 +5,9 @@ package DebugExercise;
  * Code adapted from https://stackoverflow.com/questions/4895173/bitwise-multiply-and-add-in-java and https://stackoverflow.com/questions/1533131/what-useful-bitwise-operator-code-tricks-should-a-developer-know-about
  */
 public class DebugExercise2 {
-    /** Returns the max of a and b. Do not step into this function. */
+    /**
+     * Returns the max of a and b. Do not step into this function.
+     */
     public static int max(int a, int b) {
         int w = (b - a) >> 31;
         /* If you're stepping into this function, click the
@@ -17,7 +19,9 @@ public class DebugExercise2 {
     }
 
 
-    /** Returns the sum of a and b. Do not step into this function. */
+    /**
+     * Returns the sum of a and b. Do not step into this function.
+     */
     public static int add(int a, int b) {
         int x = a, y = b;
         /* If you're stepping into this function, click the
@@ -35,10 +39,11 @@ public class DebugExercise2 {
         return xor;
     }
 
-    /** Returns a new array where entry i is the max of
+    /**
+     * Returns a new array where entry i is the max of
      * a[i] and b[i]. For example, if a = {1, -10, 3}
      * and b = {0, 20, 5}, this function will return {1, 20, 5}.
-     * */
+     */
     public static int[] arrayMax(int[] a, int[] b) {
         if (a.length != b.length) {
             System.out.println("ERROR! Arrays don't match");
@@ -46,28 +51,40 @@ public class DebugExercise2 {
         }
         int[] returnArray = new int[a.length];
         for (int i = 0; i < a.length; i += 1) {
-            int biggerValue = max(a[i], b[i]);
+            //int biggerValue = max(a[i], b[i]);
+            // max is actually return min value
+            int biggerValue;
+            if (a[i] == max(a[i], b[i])) {
+                biggerValue = b[i];
+            } else {
+                biggerValue = a[i];
+            }
             returnArray[i] = biggerValue;
         }
 
         return returnArray;
     }
 
-    /** Returns the sum of all elements in x. */
+    /**
+     * Returns the sum of all elements in x.
+     */
     public static int arraySum(int[] x) {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            //sum = sum + add(sum, x[i]);
+            // add 'sum' twice
+            sum = add(sum, x[i]);
             i = i + 1;
         }
         return sum;
     }
 
-    /** Returns the sum of the element-wise max of a and b.
-     *  For example if a = {2, 0, 10, 14} and b = {-5, 5, 20, 30},
-     *  the result should be 57.
-     * */
+    /**
+     * Returns the sum of the element-wise max of a and b.
+     * For example if a = {2, 0, 10, 14} and b = {-5, 5, 20, 30},
+     * the result should be 57.
+     */
     public static int sumOfElementwiseMaxes(int[] a, int[] b) {
         int[] maxes = arrayMax(a, b);
         int sumofMaxes = arraySum(maxes);
