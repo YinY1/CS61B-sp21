8d6455c7653b8cb359afb7879646fa8474940616
Kal'tsit <36351545+YinY1@users.noreply.github.com>
2022-09-15 22:17:15 +0800

    changed for lab4 step5

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,15 +3,8 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+        return n % 2 == 0 ? n/2 : 3*n + 1;
     }
 
     public static void main(String[] args) {
