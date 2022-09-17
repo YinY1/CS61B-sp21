ffa91a09a7f2799b7f9f533235741d65720b5fa9
Kal'tsit <36351545+YinY1@users.noreply.github.com>
2022-09-17 16:25:09 +0800

    fixed bugs and passed autograder.

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -1,35 +1,40 @@
 package tester;
+
 import static org.junit.Assert.*;
 
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import student.StudentArrayDeque;
-public class TestArrayDequeEC {
 
+public class TestArrayDequeEC {
     @Test
-    public void test1(){
-        int N = 100000;
-        StudentArrayDeque<Integer> a = new StudentArrayDeque<>();
-        StudentArrayDeque<Integer> b = new StudentArrayDeque<>();
-        for (int i = 0; i < N; i += 1) {
-            int operationNumber = StdRandom.uniform(0, 4);
-            if (operationNumber == 0) {
-                // addLast
-                int randVal = StdRandom.uniform(0, 100);
-                a.addLast(randVal);
-                b.addLast(randVal);
-            } else if (operationNumber == 1) {
-                // size
-                assertEquals(a.size(),b.size());
-            } else if (operationNumber == 2) {
-                int size = a.size();
-                if (size > 0) {
-                    assertEquals(a.removeLast(),b.removeLast());
-                }
+    public void testFailed() {
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+        String msg = "";
+        for (int i = 0; i < StdRandom.uniform(0, 1000000); i++) {
+            double choice = StdRandom.uniform();
+            Integer randVal = StdRandom.uniform(0, 100);
+            if (choice < 0.33) {
+                student.addLast(randVal);
+                solution.addLast(randVal);
+                msg += "addLast(" + randVal + ")\n";
+            } else if (choice < 0.67) {
+                student.addFirst(randVal);
+                solution.addFirst(randVal);
+                msg += "addFirst(" + randVal + ")\n";
             } else {
-                int randVal = StdRandom.uniform(0, 100);
-                a.addFirst(randVal);
-                b.addFirst(randVal);
+                int size = student.size();
+                msg += "size()\n";
+                if(size > 0){
+                    if(randVal <50){
+                        msg+="removeFirst()\n";
+                        assertEquals(msg,solution.removeFirst(),student.removeFirst());
+                    } else {
+                        msg +="removeLast()\n";
+                        assertEquals(msg,solution.removeLast(),student.removeLast());
+                    }
+                }
             }
         }
     }
