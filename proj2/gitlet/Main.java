f5548654827b7e67c174103f4077d858a2e61da4
Kal'tsit <36351545+YinY1@users.noreply.github.com>
2022-09-24 18:33:12 +0800

    created 'init()'.

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,24 +1,46 @@
 package gitlet;
 
-/** Driver class for Gitlet, a subset of the Git version-control system.
- *  @author TODO
+import java.io.IOException;
+
+/**
+ * Driver class for Gitlet, a subset of the Git version-control system.
+ *
+ * @author Edward Tsang
  */
 public class Main {
 
-    /** Usage: java gitlet.Main ARGS, where ARGS contains
-     *  <COMMAND> <OPERAND1> <OPERAND2> ... 
+    /**
+     * Usage: java gitlet.Main ARGS, where ARGS contains
+     * <COMMAND> <OPERAND1> <OPERAND2> ...
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO: what if args is empty?
+        if (args.length == 0) {
+            System.out.println("Please enter a command.");
+            System.exit(0);
+        }
         String firstArg = args[0];
-        switch(firstArg) {
+        switch (firstArg) {
             case "init":
                 // TODO: handle the `init` command
+                init();
                 break;
             case "add":
                 // TODO: handle the `add [filename]` command
                 break;
             // TODO: FILL THE REST IN
+            default:
+                System.out.println("No command with that name exists.");
+                System.exit(0);
         }
+    }
+
+    private static void init() throws IOException {
+        Repository.initializeRepo();
+        Commit commit = new Commit("initial commit",null);
+    }
+
+    private static void add() throws IOException{
+        // TODO: read all files in staging area, and then add them.
     }
 }
