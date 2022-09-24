a2e093747417361dcbf6bd8824f9ef5b670b6db1
Kal'tsit <36351545+YinY1@users.noreply.github.com>
2022-09-24 17:02:39 +0800

    created DIRs, 'initializeRepo()'.

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,29 +1,84 @@
 package gitlet;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
 
-/** Represents a gitlet repository.
+/**
+ * Represents a gitlet repository.
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author TODO
+ * @author Edward Tsang
  */
-public class Repository {
-    /**
-     * TODO: add instance variables here.
-     *
-     * List all instance variables of the Repository class here with a useful
-     * comment above them describing what that variable represents and how that
-     * variable is used. We've provided two examples for you.
+public class Repository implements Serializable {
+    /*
+      TODO: add instance variables here.
+      List all instance variables of the Repository class here with a useful
+      comment above them describing what that variable represents and how that
+      variable is used. We've provided two examples for you.
      */
 
-    /** The current working directory. */
+    /**
+     * The current working directory.
+     */
     public static final File CWD = new File(System.getProperty("user.dir"));
-    /** The .gitlet directory. */
+
+    /**
+     * The .gitlet directory.
+     */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
+    /**
+     * The staging directory.
+     */
+    public static final File STAGING_DIR = join(GITLET_DIR, "index");
+
+    /**
+     * Staged for addition.
+     */
+    public static final File ADDITION_DIR = join(STAGING_DIR, "addition");
+
+    /**
+     * Staged for removal.
+     */
+    public static final File REMOVAL_DIR = join(STAGING_DIR, "removal");
+
+    /**
+     * The blobs' directory.
+     */
+    public static final File BLOBS_DIR = join(GITLET_DIR, "objects");
+
+    /**
+     * The commit directory.
+     */
+    public static final File COMMITS_DIR = join(GITLET_DIR, "commits");
+
+    /**
+     * The branch directory.
+     */
+    public static final File BRANCHES_DIR = join(GITLET_DIR, "branches");
+
+    public static final File HEAD = join(GITLET_DIR, "HEAD");
+
     /* TODO: fill in the rest of this class. */
+    public static void initializeRepo() {
+        File[] Dir = {GITLET_DIR, STAGING_DIR, ADDITION_DIR, REMOVAL_DIR, BLOBS_DIR, COMMITS_DIR, BRANCHES_DIR};
+        for (File f : Dir) {
+            f.mkdir();
+        }
+        File f = HEAD;
+        if(!f.exists())
+        {
+            try {
+                f.createNewFile();
+            } catch (IOException exception) {
+                System.exit(0);
+            }
+        }
+    }
 }
