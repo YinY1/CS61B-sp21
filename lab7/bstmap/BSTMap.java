f9ae92a967848997b3f0191f19964d5684717ffd
Kal'tsit <36351545+YinY1@users.noreply.github.com>
2022-09-25 20:14:46 +0800

    Implemented 'iterator()', 'keySet()', 'remove()', and modified other methods

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,9 +1,12 @@
 package bstmap;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
-public class BSTMap<K extends Comparable, V> implements Map61B {
+/* Using loop as far as possible*/
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterable<K> {
 
     private class BSTNode {
         private final K key;
@@ -18,14 +21,16 @@ public class BSTMap<K extends Comparable, V> implements Map61B {
             this.right = null;
         }
 
-        private void printInOrder(){
-            if(left!=null){
-                left.printInOrder();
+        private List<BSTNode> nodesInOrder() {
+            List<BSTNode> keys = new ArrayList<>();
+            if (left != null) {
+                keys.addAll(left.nodesInOrder());
             }
-            System.out.print(val);
-            if(right!=null){
-                right.printInOrder();
+            keys.add(this);
+            if (right != null) {
+                keys.addAll(right.nodesInOrder());
             }
+            return keys;
         }
     }
 
@@ -34,8 +39,7 @@ public class BSTMap<K extends Comparable, V> implements Map61B {
     private int size;
 
     public BSTMap() {
-        this.root = null;
-        this.size = 0;
+        clear();
     }
 
     @Override
@@ -45,43 +49,25 @@ public class BSTMap<K extends Comparable, V> implements Map61B {
     }
 
     @Override
-    public boolean containsKey(Object key) {
-        BSTNode node = this.root;
-        while (node != null) {
-            if (node.key.compareTo(key) > 0) {
-                node = node.left;
-            } else if (node.key.compareTo(key) < 0) {
-                node = node.right;
-            } else {
-                return true;
-            }
+    public boolean containsKey(K key) {
+        if (this.root == null) {
+            return false;
         }
-        return false;
+        Object[] obj = bSearch(this.root, key);
+        return obj[2].equals(0);
     }
 
     @Override
-    public Object get(Object key) {
+    public V get(K key) {
         if (this.root == null) {
             return null;
         }
-        BSTNode node = this.root;
-        while (true) {
-            if (node.key.compareTo(key) > 0) {
-                if(node.left!=null){
-                    node = node.left;
-                } else {
-                    return null;
-                }
-            } else if (node.key.compareTo(key) < 0) {
-                if(node.right != null ){
-                    node = node.right;
-                } else {
-                    return null;
-                }
-            } else {
-                return node.val;
-            }
+        Object[] obj = bSearch(this.root, key);
+        BSTNode node = (BSTNode) obj[1];
+        if (obj[2].equals(0)) {
+            return node.val;
         }
+        return null;
     }
 
     @Override
@@ -90,58 +76,137 @@ public class BSTMap<K extends Comparable, V> implements Map61B {
     }
 
     @Override
-    public void put(Object key, Object value) {
+    public void put(K key, V value) {
         if (this.root == null) {
-            this.root = new BSTNode((K) key, (V) value);
+            this.root = new BSTNode(key, value);
             this.size++;
             return;
         }
         BSTNode node = this.root;
+        Object[] obj = bSearch(node, key);
+        node = (BSTNode) obj[1];
+        if (obj[2].equals(0)) {
+            return;
+        }
+        if (obj[2].equals(-1)) {
+            node.left = new BSTNode(key, value);
+        } else {
+            node.right = new BSTNode(key, value);
+        }
+        this.size++;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        if (this.size == 0) {
+            return null;
+        }
+        List<K> keys = new ArrayList<>();
+        for (BSTNode node : this.root.nodesInOrder()) {
+            keys.add(node.key);
+        }
+        return Set.copyOf(keys);
+    }
+
+    @Override
+    public V remove(K key) {
+        Object[] obj = bSearch(this.root, key);
+        BSTNode prev = (BSTNode) obj[0];
+        BSTNode node = (BSTNode) obj[1];
+        BSTNode rm = node;
+        V ret = rm.val;
+        if (node == this.root) {
+            removeRoot(node, rm);
+        } else if (node.left == null && node.right == null) {
+            if (obj[2].equals(-1)) {
+                prev.left = null;
+            } else {
+                prev.right = null;
+            }
+        } else {
+            node = findRightMostLeft(node, rm);
+            if (obj[2].equals(-1)) {
+                prev.left = node;
+            } else {
+                prev.right = node;
+            }
+        }
+        this.size--;
+        return ret;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        return remove(key);
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return keySet().iterator();
+    }
+
+    public void printInOrder() {
+        StringBuilder s = new StringBuilder();
+        for (BSTNode node : this.root.nodesInOrder()) {
+            s.append(node.val.toString());
+        }
+        System.out.print(s);
+    }
+
+    private Object[] bSearch(BSTNode node, K key) {
+        BSTNode prev = null;
         while (true) {
             if (node.key.compareTo(key) > 0) {
                 if (node.left != null) {
+                    prev = node;
                     node = node.left;
                 } else {
-                    node.left = new BSTNode((K) key, (V) value);
-                    this.size++;
-                    return;
+                    // next will be left child
+                    return new Object[]{prev, node, -1};
                 }
             } else if (node.key.compareTo(key) < 0) {
                 if (node.right != null) {
+                    prev = node;
                     node = node.right;
                 } else {
-                    node.right = new BSTNode((K) key, (V) value);
-                    this.size++;
-                    return;
+                    // next will be right child
+                    return new Object[]{prev, node, 1};
                 }
             } else {
-                return;
+                // reach the target
+                return new Object[]{prev, node, 0};
             }
         }
     }
 
-
-    @Override
-    public Set keySet() throws UnsupportedOperationException {
-        return null;
+    private BSTNode findRightMostLeft(BSTNode node, BSTNode rm) {
+        if (node.left == null) {
+            node = node.right;
+        } else if (node.right == null) {
+            node = node.left;
+        } else {
+            node = node.left;
+            if (node.right != null) {
+                BSTNode pn = null;
+                while (node.right != null) {
+                    pn = node;
+                    node = node.right;
+                }
+                pn.right = node.left;
+                node.left = rm.left;
+            }
+            node.right = rm.right;
+        }
+        return node;
     }
 
-    @Override
-    public Object remove(Object key) throws UnsupportedOperationException {
-        return null;
-    }
-
-    @Override
-    public Object remove(Object key, Object value) throws UnsupportedOperationException {
-        return null;
-    }
-
-    @Override
-    public Iterator iterator() throws UnsupportedOperationException {
-        return null;
-    }
-
-    public void printInOrder(){
-        this.root.printInOrder();
+    private void removeRoot(BSTNode node, BSTNode rm) {
+        if (node.left != null) {
+            node = findRightMostLeft(node, rm);
+            node.right = rm.right;
+            this.root = rm.left;
+        } else {
+            this.root = this.root.right;
+        }
     }
 }
