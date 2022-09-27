507a0be0012cd5a4c75be2f7ff11d5dbebf26ff4
Kal'tsit <36351545+YinY1@users.noreply.github.com>
2022-09-27 23:27:11 +0800

    Modified structure of class `Blob`.

--- proj2/gitlet/Blob.java
+++ proj2/gitlet/Blob.java
@@ -1,16 +1,54 @@
 package gitlet;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.List;
+import java.util.TreeMap;
 
+import static gitlet.Repository.*;
 import static gitlet.Utils.*;
 
 public class Blob implements Serializable {
-    private final byte[] content;
+    final byte[] content;
     File file;
 
     public Blob(File f) {
         this.content = readContents(f);
         this.file = f;
+    }
+
+    public static void readBlobs(File DIR) {
+        List<String> names = plainFilenamesIn(DIR);
+        Repository.blobs = new TreeMap<>();
+        for (String name : names) {
+            File blob = join(DIR, name);
+            Blob b = readObject(blob, Blob.class);
+            Repository.blobs.put(b.file, name);
+        }
+    }
+
+    public static void makeBlob(Blob b) {
+        String name = getBlobName(b.file);
+        File out = join(TEMP_BLOBS_DIR, name);
+        writeObject(out, b);
+    }
+
+    public static String getBlobName(File f) {
+        String name = readContentsAsString(f)+f.getName();
+        return sha1(name);
+    }
+
+    public static void moveBlobs(TreeMap<File, String> blobs) throws IOException {
+        //move blobs from temp to BLOB_DIR
+        for (String b : blobs.values()) {
+            // first copy them
+            File tempBlob = join(TEMP_BLOBS_DIR, b);
+            writeFile(tempBlob, BLOBS_DIR, b);
+            // then delete them
+            tempBlob.delete();
+        }
+
+
     }
 }
